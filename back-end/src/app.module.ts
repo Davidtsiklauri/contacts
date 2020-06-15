b22fbfc6b3e151566377b40576d2f00c886d7345
Davidtsiklauri <wiklauri.dato1997@gmail.com>
2020-06-15 11:50:28 +0400

    //

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import 'dotenv/config';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URI)
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
