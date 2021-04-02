11e084a39161fe0fbd97e6325823c7c5e9f6f72e
Davidtsiklauri <wiklauri.dato1997@gmail.com>
2021-04-02 22:40:30 +0400

    added db need to add env vars

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -8,13 +8,12 @@ import { AuthModule } from './auth/auth.module';
 import { ContactsModule } from './contacts/contacts.module';
 import { GroupsModule } from './groups/groups.module';
 import { UploadModule } from './upload/upload.module';
-import { MailerModule } from '@nestjs-modules/mailer'
-
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGO_URI),
+    MongooseModule.forRoot('mongodb://root:example@mongo:27017'),
     UserModule,
     AuthModule,
     ContactsModule,
@@ -22,17 +21,17 @@ import { MailerModule } from '@nestjs-modules/mailer'
     UploadModule,
     MailerModule.forRoot({
       transport: {
-        service: "gmail.com",
+        service: 'gmail.com',
         host: 'localhost',
         port: 1025,
         ignoreTLS: true,
         secure: false,
         auth: {
           user: process.env.MAILDEV_INCOMING_USER,
-          pass: process.env.MAILDEV_INCOMING_PASS
+          pass: process.env.MAILDEV_INCOMING_PASS,
         },
       },
-    })
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
